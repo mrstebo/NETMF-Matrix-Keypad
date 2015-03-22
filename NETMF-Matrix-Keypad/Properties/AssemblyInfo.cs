674322433922d4e00d814a381c8d95bc2a5df8c0
mrstebo <steven.atkinson@gmx.com>
2015-03-22 09:40:33 +0000

    Updated assembly information

--- NETMF-Matrix-Keypad/Properties/AssemblyInfo.cs
+++ NETMF-Matrix-Keypad/Properties/AssemblyInfo.cs
@@ -1,16 +1,14 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NETMF-Matrix-Keypad")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Example program for integrating the FEZ Panda II with a matrix keypad")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("mrstebo - http://mrstebo.co.uk/")]
 [assembly: AssemblyProduct("NETMF-Matrix-Keypad")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
